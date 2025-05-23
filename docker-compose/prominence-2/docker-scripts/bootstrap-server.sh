498af7d0e306e4fbcb27f28bf34ea6be9994d0d7
Henry Post <HenryFBP@gmail.com>
2025-05-23 17:54:36 -0500

    fix root ownership issue by using exec and su

--- docker-compose/prominence-2/docker-scripts/bootstrap-server.sh
+++ docker-compose/prominence-2/docker-scripts/bootstrap-server.sh
@@ -5,7 +5,6 @@ echo $(id)
 echo "User ID: $(id -u minecraft)" && \
 echo "Group ID: $(id -g minecraft)"
 
-
 # Define source and target directories
 SOURCE_DIR="/minecraft-unzipped" # we don't have a subpath here because this specific modpack author unzips the server files into the root of the zip
 TARGET_DIR="/minecraft"
@@ -21,6 +20,18 @@ fi
 # Accept the EULA
 echo "eula=true" > "$TARGET_DIR/eula.txt"
 
-# Start the server
-echo "Starting the Minecraft server..."
-exec bash "$TARGET_DIR/start.sh"
+# Start the server as the minecraft user
+echo "Changing ownership of $TARGET_DIR to user 'minecraft'..."
+chown -R minecraft:minecraft "$TARGET_DIR"
+
+echo "Setting permissions for $TARGET_DIR..."
+chmod -R 755 "$TARGET_DIR"
+
+# print useful info and sleep
+ls -lash /minecraft
+echo "Sleeping for 5 seconds to allow for debugging..."
+sleep 5
+
+echo "Starting the server as the minecraft user..."
+# Switch to the minecraft user and execute the start script
+exec su -c "bash $TARGET_DIR/start.sh" minecraft
