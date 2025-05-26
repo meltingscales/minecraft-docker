2f120b0e63cbb722f7e7acaadf4a797678d72711
Henry Post <HenryFBP@gmail.com>
2025-05-25 21:23:28 -0500

    make backup script

--- scripts/backup-all.sh
+++ scripts/backup-all.sh
@@ -1,12 +1,42 @@
 #!/usr/bin/env bash
-
+set -euxo pipefail
 
 # This script is used to backup all the minecraft-data folders and timestamp them.
 
 BACKUP_SRC="~/Git/minecraft-docker/docker-compose/"
+# expand backup source
+BACKUP_SRC=$(eval echo "$BACKUP_SRC")
+
 BACKUP_DEST="~/minecraft-backup/"
+BACKUP_DEST=$(eval echo "$BACKUP_DEST")
+
 BACKUP_NAME="backup-$(date +%Y-%m-%d_%H-%M-%S).tar.gz"
 
-# TODO finish the script
-# TODO add a check to see if the backup destination exists
-# TODO add a check to see if the backup source exists
+if [ ! -d "$BACKUP_DEST" ]; then
+    echo "Backup destination does not exist. Creating it..."
+    mkdir -p "$BACKUP_DEST"
+fi
+
+# set globbing mode to match directories
+shopt -s nullglob
+
+dirs="$BACKUP_SRC*/minecraft-data"
+# loop over all minecraft-data folders
+for dir in $dirs; do
+
+    if [ -d "$dir" ]; then
+        echo "Backing up $dir"
+
+        # get name of the modpack
+        MODPACK_NAME=$(basename "$(dirname "$dir")")
+        
+        # create a backup name with the modpack name
+        BACKUP_NAME="backup-${MODPACK_NAME}-$(date +%Y-%m-%d_%H-%M-%S).tar.gz"
+        # create the backup
+        sudo tar -czf "$BACKUP_DEST$BACKUP_NAME" -C "$dir" .
+        sudo chown "$USER" "$BACKUP_DEST$BACKUP_NAME"
+        echo "Backup created: $BACKUP_DEST$BACKUP_NAME"
+    else
+        echo "No minecraft-data directory found in $dir"
+    fi
+done
